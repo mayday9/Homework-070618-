533ce6ba9e691aa1efa71ca27f5edb1ee17acf35
mayday9 <themaydaygmt@gmail.com>
2018-06-07 20:55:51 +0300

    a-500

---
+++ script.js
@@ -0,0 +1,33 @@
+/*Задание 1
+
+var start, counter, summ=0;
+
+start=parseInt(prompt("Введете целое число:"));
+counter=start;
+
+for(counter; counter<=500; counter++){
+ //   if(start==500){
+ //       alert("Сумма равна 500.");
+ //       break;
+ //   } else {
+    summ=summ+counter;
+ //   }
+}
+alert("Сумма целых чисел от: "+start+" до 500 равна "+summ);*/
+
+/*Задание 2
+
+var x, x, summ;
+
+x=parseInt(prompt("Введете целое число Х:"));
+y=parseInt(prompt("Введете целое число Y:"));
+
+summ=Math.pow(x, y);
+
+alert(x+" в степени "+y+" равен: "+summ); */
+
+//Задание 3
+
+
+
+
